ccd661f1583fa429689a9b19d73398655d79d068
Valmo <val_mo_jr@hotmail.com>
2022-06-23 15:46:26 -0300

    primeira versão

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import './style.css'
+import profile from './profile.png'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render (
+    <body>
+    <div className="frame">
+        <h1>Valmo Trindade</h1>
+        <h2>Dev</h2>
+        <img src={profile} id="profile"/>
+        <p>Um cara legal</p>
+    </div>
+    </body>,
+    document.getElementById('root')
+)
