f06d41485dbf3b8bb0122a2082611c9fcc89c068
Valmo <val_mo_jr@hotmail.com>
2022-07-21 22:18:33 -0300

    updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Apresentação Pessoal
+- I'm Valmo
+- I'm a former army officer with a military science degree
+- Currently, i am software engineering and military law master's student.
+- I've always tried to take advantage of IT tools, so at this very moment i'm learning HTML, CSS and JS, aiming to get a Front-End Dev profile.
+- Personal interest in studying public safety, geopolitics, UX and C2 tools.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './style.css'
-import profile from './profile.png'
-
 ReactDOM.render (
     <body>
     <div className="frame">
         <h1>Valmo Trindade</h1>
-        <h2>Dev</h2>
-        <img src={profile} id="profile"/>
-        <p>Um cara legal</p>
+        <h2>Front End React Dev</h2>
+        <div id="profile"></div>
+        <p>23, Former Military, CS degree student, passionate about the unlimitations of multi plaform web applications and how can we change our enviroment with then.</p>
     </div>
     </body>,
     document.getElementById('root')
